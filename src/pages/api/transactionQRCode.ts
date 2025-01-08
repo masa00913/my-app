8031d5faee6bbe917f4285a9380a5383f1f97200
MASA_K\masa2 <masa20040913@gmail.com>
2025-01-09 00:54:49 +0900

    ファイル名を修正

---
+++ src/pages/api/transactionQRCode.ts
@@ -0,0 +1,49 @@
+// pages/api/exchangePoints.ts
+import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ success: false, message: 'Method Not Allowed' });
+  }
+
+  const { qrCodeData, fromUser, toUser } = req.body;
+
+  if (!qrCodeData) {
+    return res.status(400).json({ success: false, message: 'QRコードデータがありません' });
+  }
+  console.log('tryに入る');
+  try {
+    // ここで QRコードデータ (`qrCodeData`) を検証し、
+    // データベースなどを参照してポイント交換処理を行う
+    // 例: ユーザーIDの抽出、ポイントの減算など
+
+    // ダミーの処理とレスポンス
+    // const exchangedPoints = 100; // QRコードに関連付けられたポイント数
+    // const currentBalance = 500; // データベースから現在の残高を取得する
+    const sender = await prisma.user.findUnique({
+      where: { username: fromUser },
+    });
+
+    const recipient = await prisma.user.findUnique({
+      where: { username: toUser },
+    });
+
+    if (!sender || !recipient) {
+      return res.status(404).json({ error: 'ユーザーが見つかりません。' });
+    }
+
+    console.log('成功');
+    // 成功レスポンス
+    return res.status(200).json({
+      success: true,
+    });
+  } catch (error) {
+    console.error('ポイント交換エラー:', error);
+    return res.status(500).json({ success: false, message: 'ポイント交換処理中にエラーが発生しました' });
+  }
+}
